f00a31f89351df1f0ba4c1b003e83480f37fc086
jonny <coorhook@gmail.com>
2025-03-31 13:35:10 +0800

    Update settings.js

--- openwrt/luci-app-cloud-clipboard/htdocs/luci-static/resources/view/cloud-clipboard/settings.js
+++ openwrt/luci-app-cloud-clipboard/htdocs/luci-static/resources/view/cloud-clipboard/settings.js
@@ -4,15 +4,14 @@
 'require uci';
 'require fs';
 'require ui';
-'require tools.widgets as widgets';
 'require rpc';
 
 return view.extend({
     render: function() {
         var m, s, o;
-
         m = new form.Map('cloud-clipboard', _('Cloud Clipboard'), _('Cloud Clipboard是一个文本和文件传输工具，可在多个设备之间共享剪贴板内容。'));
 
+        // 基本设置
         s = m.section(form.TypedSection, 'cloud-clipboard', _('基本设置'));
         s.anonymous = true;
 
@@ -33,11 +32,12 @@ return view.extend({
         o.password = true;
         o.rmempty = true;
         o.description = _('如果设置，访问时需要输入此密码。留空表示不需要密码。');
-        
-        // 添加状态节
-        s = m.section(form.TypedSection, 'cloud-clipboard', _('服务状态'));
+
+        // 服务状态与重启服务放在一个单独的 section，下方状态显示在重启按钮正上方
+        s = m.section(form.TypedSection, 'cloud-clipboard-status', _('服务状态'));
         s.anonymous = true;
-        
+
+        // 运行状态显示
         o = s.option(form.DummyValue, '_status', _('运行状态'));
         o.rawhtml = true;
         o.load = function() {
@@ -53,13 +53,14 @@ return view.extend({
                 return E('span', { 'class': 'label danger' }, [ _('状态未知') ]);
             });
         };
-        
+
+        // 重启服务按钮
         o = s.option(form.Button, '_restart', _('重启服务'));
         o.inputtitle = _('重启服务');
         o.inputstyle = 'apply';
         o.onclick = function() {
             return fs.exec('/etc/init.d/cloud-clipboard', ['restart']).then(function() {
-                ui.addNotification(null, E('p', _('已重启Cloud Clipboard服务')));
+                ui.addNotification(null, E('p', _('已重启 Cloud Clipboard 服务')));
                 window.setTimeout(function() {
                     location.reload();
                 }, 1000);
