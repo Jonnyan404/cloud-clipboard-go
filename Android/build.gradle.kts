79cb76a5fb96b15aa00de38e52e5b5eab62c4541
jonny <coorhook@gmail.com>
2025-05-07 11:17:17 +0800

    feat: 添加Android应用的基本配置，包括命名空间、SDK版本和构建类型

--- Android/build.gradle.kts
+++ Android/build.gradle.kts
@@ -5,3 +5,24 @@ plugins {
     // 如果使用 Kotlin
     // id("org.jetbrains.kotlin.android") version "1.9.0" apply false // 示例版本
 }
+android {
+    namespace = "com.yourdomain.cloudclipboard" // 替换为你的实际包名
+    compileSdk = 34 // 或者你项目使用的 SDK 版本
+
+    defaultConfig {
+        applicationId = "com.jonnyan404.cloudclipboard" // 替换为你的实际应用 ID
+        minSdk = 24 // 或者你项目支持的最低 SDK
+        targetSdk = 34 // 或者你项目针对的 SDK
+        versionCode = 1
+        versionName = "1.0"
+        // ... 其他配置 ...
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    // ... 其他 android 配置 ...
+}
