45e9e4bac920a6e96f03dc27ca6cd4457efa73c9
jonny <coorhook@gmail.com>
2026-09-20 22:36:21 +0800

    build(web): rebuild the embedded static assets
    Regenerated with `DEPLOY_STATIC=1 npm run build` so `cloud-clip/lib/static`
    matches the group switches, the relocated search bar and the send-button rule.

--- cloud-clip/lib/static/assets/i18n-DhUomM_X.js
+++ cloud-clip/lib/static/assets/i18n-DhUomM_X.js
@@ -1,4 +1,4 @@
-import{B as wn,z as at,i as Un,r as Wn,s as $n,c as Ie,d as gt,h as ln,F as cn,l as Ne,m as Vn,o as Hn,a0 as Yn,n as xn,T as Gn}from"./vue-core-CijL4G-c.js";/*!
+import{B as wn,z as at,i as Un,r as Wn,s as $n,c as Ie,d as gt,h as ln,F as cn,l as Ne,m as Vn,o as Hn,a0 as Yn,n as xn,T as Gn}from"./vue-core-B5utjIDw.js";/*!
   * shared v10.0.8
   * (c) 2025 kazuya kawaguchi
   * Released under the MIT License.
